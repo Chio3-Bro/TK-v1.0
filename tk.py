28b06bd26c2b13701d95e6c1cd4ab633fd9ef750
Chio-Bro <87470626+Chio3-Bro@users.noreply.github.com>
2021-08-08 09:42:21 +0300

    Add files via upload
    It`s version tk1.0.py

---
+++ tk.py
@@ -0,0 +1,283 @@
+from tkinter import *
+import time
+
+root = Tk()
+ 
+clicks = 0
+cmd_list = 'Commands:\n1.ddos,\n2.ipv4,\n3.kill_pc,\n4.mac,\n5.logger,\n6.@mail,\n7.scam,\n8.mob'
+mac = '34-55-33-45-FD-46'
+cn = 'XXXX-XXXX-XXXX-XXXX'
+
+root['bg'] = '#fafafa'
+root.title('Login')
+root.geometry('300x150')
+
+def click_button():
+	global clicks
+	clicks +=1
+	if clicks == 1:
+		print('--> "rootilak" Logged With Password <-- ')
+	root.destroy()
+	print('Enter owners username:')
+	uname = input()
+	
+	print(cmd_list)
+	print('Enter func:')
+	func = input()
+
+	if gunc == 'mob':
+		print('Enter mobile number with operator(UA +380; RUS +7):')
+		mp = input()
+		if mp == '+3800000000000' or '+7000000000':
+			print('Error code: 0x05')
+		else:
+			print('SMS-b0mb3r start process')
+			time.sleep(6)
+			print('Succesful! B0mB3r complete mission!!')
+
+	if func == 'scam':
+		print('Card has format: '+ cn)
+		print('Enter card number:')
+		cn1 = input()
+		if cn1 == '1234-5678-9012-3456':
+			print('Error code: 0x02')
+		else:
+			print('We need PIN-code to attach bank')
+			print('Enter PIN-code:')
+			pn = input()
+			if pn == '1234':
+				print('Error code: 0x03')
+			else:
+				print('Wait 5-10 seconds...')
+				time.sleep(7.5)
+				print('Error code: 0x04')
+				time.sleep(2)
+
+
+
+	if func == '@mail':
+		print('Type victim e-mail:')
+		email = input()
+		print('#> Hacking ' + email)
+		print('10%')
+		time.sleep(0.5)
+		print('20%')
+		time.sleep(0.5)
+		print('IP hacked...')
+		time.sleep(0.5)
+		print('30%')
+		time.sleep(0.5)
+		print('40%')
+		time.sleep(0.5)
+		print('Login hacked...')
+		time.sleep(0.5)
+		print('50%')
+		time.sleep(0.5)
+		print('60%')
+		time.sleep(0.5)
+		print('Messages complete moderation...')
+		time.sleep(0.5)
+		print('70%')
+		time.sleep(0.5)
+		print('80%')
+		time.sleep(0.5)
+		print('Password Taken')
+		time.sleep(0.5)
+		print('90%')
+		time.sleep(0.5)
+		print('100%')
+		time.sleep(0.5)
+		print('#> Well done! E-mail ' + email + ' Hacked')
+		print('Generation copy of data base...')
+		print('Do you want to continue? [y/n]')
+		a = input()
+		if a == 'y' :
+			print('IP: 116.214.48.211 ')
+			print('MAC-Address: 34-55-33-45-FD-46')
+			print('Choose command:')
+			print('Bomber(b0mb3r)\n Delete Data(dd)\n BruteForce to connected accounts(bt)')
+			b = input()
+			if b == 'b0mb3r':
+				print('Amount: ')
+				c = input(), print('Wait about 5 sec-s')
+				time.sleep(5)
+				print('Well Done!')
+				print('Server will be closed in ')
+				print('5')
+				time.sleep(1)
+				print('4 ')
+				time.sleep(1)
+				print('3 ')
+				time.sleep(1)
+				print('2 ')
+				time.sleep(1)
+				print('1 ')
+				time.sleep(1)
+			if b == 'dd':
+				print('Deliting data in 5 seconds...')
+				print('5')
+				time.sleep(1)
+				print('4 ')
+				time.sleep(1)
+				print('3 ')
+				time.sleep(1)
+				print('2 ')
+				time.sleep(1)
+				print('1 ')
+				time.sleep(1)
+				print('Well Done!')
+				print('Server will be closed in ')
+				print('5')
+				time.sleep(1)
+				print('4 ')
+				time.sleep(1)
+				print('3 ')
+				time.sleep(1)
+				print('2 ')
+				time.sleep(1)
+				print('1 ')
+				time.sleep(1)
+			if b == 'bt':
+				print('Ctrl+C and Ctrl+V this: qwertyuiopasdfghjklzxcvbnm1234567890')
+				time.sleep(3)
+				print('Input please alphabet!!!')
+				d = input()
+				print('Hacking ' + email + '1%')
+				print('Hacking ' + email + '2%')
+				print('Hacking ' + email + '3%')
+				print('Hacking ' + email + '4%')
+				print('Hacking ' + email + '5%')
+				print('Hacking ' + email + '6%')
+				print('Hacking ' + email + '7%')
+				print('Hacking ' + email + '8%')
+				print('Hacking ' + email + '9%')
+				print('Hacking ' + email + '10%')
+				print('Hacking ' + email + '11%')
+				print('Hacking ' + email + '12%')
+				print('Hacking ' + email + '13%')
+				print('Hacking ' + email + '14%')
+				print('Hacking ' + email + '15%')
+				print('Hacking ' + email + '16%')
+				print('Hacking ' + email + '17%')
+				print('Hacking ' + email + '18%')
+				print('Hacking ' + email + '19%')
+				print('Hacking ' + email + '20%')
+				print('Hacking ' + email + '32%')
+				print('Hacking ' + email + '43%')
+				print('Hacking ' + email + '53%')
+				print('Hacking ' + email + '61%')
+				print('Hacking ' + email + '81%')
+				print('Hacking ' + email + '82%')
+				print('Hacking ' + email + '83%')
+				print('Hacking ' + email + '84%')
+				print('Hacking ' + email + '85%')
+				print('Hacking ' + email + '86%')
+				print('Hacking ' + email + '87%')
+				print('Hacking ' + email + '88%')
+				print('Hacking ' + email + '88%')
+				print('Hacking ' + email + '89%')
+				print('Hacking ' + email + '91%')
+				print('Hacking ' + email + '94%')
+				print('Hacking ' + email + '100%')
+				print('BruteForce is completed!')
+				print('Password HASH: \n65B27D9EAA0282A8D9E5368B3831410A')
+				print('Server closing in 5 seconds')
+				time.sleep(5)
+
+
+		if a == 'n':
+			print('#> BYE!')
+		
+
+	if func == 'logger':
+		print('#> Func KeyLogger is paid! You have to donate or input code')
+		print('#> Do u want to enter code?[y/n]:')
+		sure5 = input()
+		if sure5 == 'y':
+			print('#> Enter here')
+			ip = input()
+			if ip == '#> 164-643-8413-8521-4265':
+				print('#> Reboot pc and go more)')
+			else:
+				print('Error code: 0x04')
+		if sure5 == 'n':
+			print('#> Processes are stopped, you can close window')
+
+	if func == 'mac':
+		print('#> MAC of the victim is: ' + mac )
+		print('#> Do u want to continue(c-continue, d-close):')
+		sure4 = input()
+		if sure4 == 'c':
+			print('#> Commands ' + cmd_list)
+			print('#> Restart and choose another commands')
+		if sure4 == 'd':
+			print('#> Processes are stopped, you can close window')
+
+	if func == 'ipv4':
+		print('#> IPv4 of the ' + uname+ ' is 116.214.48.211')
+		print('#> Do u want to continue(c-continue, d-close):')
+		sure3 = input()
+		if sure3 == 'c':
+			print('#> Commands ' + cmd_list)
+			print('#> Restart and choose another commands')
+		if sure3 == 'd':
+			print('#> Processes are stopped, you can close window')
+
+	if func == 'ddos':
+		print('#> DDoS attach for User '+ uname + ' wait about 3 sec-s')
+		time.sleep(3)
+		print('#> Well done! PC was killed!')
+		print('#> Do u want to continue(c-continue, d-close):')
+		sure2 = input()
+		if sure2 == 'c':
+			print('#> Commands ' + cmd_list)
+			print('#> Restart and choose another commands')
+		if sure2 == 'd':
+			print('Processes are stopped, you can close window')
+
+	if func == 'kill_pc':
+		print('#> Are you sure want to kill pc of the User '+uname+ ' [Да/Нет][Y/N]: ')
+		sure = input()
+		if sure == 'Да' or 'Y':
+			print('#> Killing ' + uname + ' pc')
+			print('10%')
+			time.sleep(2)
+			print('20%')
+			time.sleep(2)
+			print('30%')
+			time.sleep(2)
+			print('40%')
+			time.sleep(2)
+			print('50%')
+			time.sleep(2)
+			print('60%')
+			time.sleep(2)
+			print('70%')
+			time.sleep(2)
+			print('80%')
+			time.sleep(2)
+			print('90%')
+			time.sleep(2)
+			print('100%')
+			time.sleep(2)
+			print('#> Well done! PC was killed!')
+			print('#> Do u want to continue(c-continue, d-close):')
+			sure1 = input()
+			if sure1 == 'c':
+				print('#> Commands ' + cmd_list)
+			print('#> Restart and choose another commands')
+			if sure1 == 'd':
+				print('#> Processes are stopped, you can close window')
+
+				
+r_txt1 = Label(text='Войдите в систему для взлома...').pack()
+r_txt4 = Label(text='Login: rootilak, Password:jackjam').pack()
+r_txt2 = Label(text='Login:').pack()
+r_f1 = Entry().pack()
+r_txt3 = Label(text='Password:').pack()
+r_f2 = Entry(show = '*').pack()
+btn = Button(bg='red', text='Continue [eng]', width=20,height=1, command = click_button,).pack()
+
+root.resizable(width=False, height=False)
+
+root.mainloop()
